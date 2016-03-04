739f55e928dd15ca27e03e7f463cc806379ab376
juanmnl <juanmnlcornejo@gmail.com>
2016-03-04 12:35:21 -0500

    Add project and first structure creation commands

--- bin/gitano.js
+++ bin/gitano.js
@@ -6,7 +6,7 @@ var inquirer = require('inquirer');
 
 ////* GITANO *////
 
-shell.echo('Hola, me llaman el Gitano!\nI\'ll help you setup your React project.\n');
+shell.echo('\nHola, me llaman el Gitano!\nI\'ll help you setup your React project.\n');
 setTimeout(function () {
   shell.echo('\033[34mPreparing some magic powders... \033[0m');
 }, 1200)
@@ -26,8 +26,8 @@ setTimeout(function () {
     },
     {
       type: 'list',
-      name: 'Strucure',
-      message: 'What kind of structure you like?\n',
+      name: 'Structure',
+      message: 'What kind of structure do you like?\n',
       choices: [
         'src - dist',
         'app - dist',
@@ -58,7 +58,7 @@ setTimeout(function () {
       type: 'list',
       name: 'Testing',
       message: 'Do you write tests?\n',
-      choices: [ 'Mocha + Chai', 'AVA', 'No, I live on the edge!' ]
+      choices: [ 'Mocha + Chai', 'AVA', 'No thanks, I\'m living on the edge!' ]
     }
   ];
 
@@ -67,8 +67,27 @@ setTimeout(function () {
       shell.echo('\n\033[34mThe spirits are with you! \033[0m');
     }, 700)
     setTimeout(function () {
+      // Create Project
+      var project = answers.Name;
+      shell.echo('Creating ' + project + '...');
+      shell.exec('mkdir ' + project);
+      shell.exec('cd ' + project + ' && npm init -y --loglevel=silent');
+      shell.echo('\033[32mProject created with package.json \033[0m\n');
+      // Create Structure
+      if (answers.Structure === 'src - dist') {
+          shell.echo('Creating Structure...\n');
+          shell.exec('cd ' + project + ' && mkdir src src/components');
+          shell.exec('cd ' + project + ' && mkdir dist');
+          shell.exec('find ' + project + ' -type d -print');
+          shell.echo('\033[32mStructure created \033[0m');
+      }
       console.log('\nYour Project setup:');
       console.log( JSON.stringify(answers, null, '  ') );
     }, 2000)
+    setTimeout(function () {
+      shell.echo('\n\033[32mReady! \033[0m');
+      shell.echo('\n\033[34mDon\'t forget to cd into your project and \'git init\' \033[0m');
+      shell.echo('\033[34mSee you soon! \033[0m');
+    }, 2700)
   });
-}, 3100)
+}, 3500)
